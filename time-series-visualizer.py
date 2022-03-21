0b567d67e0cdb078a4e07f2f2585fe7bfa95d8ba
Jordan S <jpstanway@gmail.com>
2022-03-20 23:11:13 -0700

    initial

---
+++ time-series-visualizer.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+# Import data (Make sure to parse dates. Consider setting index column to 'date'.)
+df = pd.read_csv('fcc-forum-pageviews.csv')
+df = pd.to_datetime(df['date'], format="%y-%m")
+df.set_index('date')
+df.rename(columns={'date': 'Date', 'value': 'Page Views'}, inplace=True)
+
+# Clean data
+df = df[df.isnull() == False]
+print(df)
+
+
+def draw_line_plot():
+    # Draw line plot
+    fig = sns.lineplot(x="date", y="Page Views", data=df).figure
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('line_plot.png')
+    return fig
+
+
+def draw_bar_plot():
+    # Copy and modify data for monthly bar plot
+    df_bar = None
+
+    # Draw bar plot
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('bar_plot.png')
+    return fig
+
+
+def draw_box_plot():
+    # Prepare data for box plots (this part is done!)
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
+    df_box['year'] = [d.year for d in df_box.date]
+    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+
+    # Draw box plots (using Seaborn)
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('box_plot.png')
+    return fig
